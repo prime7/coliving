4ba2d35a22bb227a0a469106b91e03dad0b7bf27
Tarek Ahmed <tarek5701@gmail.com>
2020-05-20 08:30:23 -0700

    Added validation in password at registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from .validators import validateEmail
 from .models import Profile
+from django.contrib.auth.password_validation import validate_password
 
 class UserRegisterForm(forms.Form):
     email = forms.EmailField(validators=[validateEmail],label='Email')
-    password1 = forms.CharField(widget=forms.PasswordInput(),label='Password')
-    password2 = forms.CharField(widget=forms.PasswordInput(),label='Confirm Password')
+    password1 = forms.CharField(validators=[validate_password],widget=forms.PasswordInput(),label='Password')
+    password2 = forms.CharField(validators=[validate_password],widget=forms.PasswordInput(),label='Confirm Password')
 
     def clean(self):
         cleaned_data = super(UserRegisterForm, self).clean()
