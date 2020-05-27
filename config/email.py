9a03db4df5167941a9d9cf2fbe2f7d880837f76c
Tarek Ahmed <tarek5701@gmail.com>
2020-05-27 13:53:29 -0700

    added decouple

---
+++ config/email.py
@@ -0,0 +1,14 @@
+from decouple import config
+
+
+if config('DEBUG'): 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
