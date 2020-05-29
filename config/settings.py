2003230ea288e3e54a4434d88ec6df546bbb64cd
Tarek Ahmed <tarek5701@gmail.com>
2020-05-28 22:02:20 -0700

    removed database from settings

--- config/settings.py
+++ config/settings.py
@@ -48,13 +48,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
