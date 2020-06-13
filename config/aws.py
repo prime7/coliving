bb390175aff6f66166b312c27912a7159fca9d3e
Tarek Ahmed <tarek5701@gmail.com>
2020-06-13 12:27:05 -0700

    Media url made common for debug

--- config/aws.py
+++ config/aws.py
@@ -21,7 +21,7 @@ if 'RDS_DB_NAME' in os.environ:
 else:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-    MEDIA_URL = '/media/'   
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_URL = '/media/'   
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root'), ]
