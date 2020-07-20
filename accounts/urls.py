fb6a89f8b82cfc7c0f249b578c54931a4495fb0b
Tarek Ahmed <tarek5701@gmail.com>
2020-07-20 12:54:00 -0700

    Html updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('for-leaseholders/', TemplateView.as_view(template_name="accounts/holders.html"),name='for-leaseholders'),
     path('privacy-policy/', TemplateView.as_view(template_name="accounts/privacy.html"),name='privacy-policy'),
     path('terms/', TemplateView.as_view(template_name="accounts/terms.html"),name='terms-and-condition'),
+    path('partners/', TemplateView.as_view(template_name="accounts/partners.html"),name='partners'),
 ]
