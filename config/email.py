15284198dd7735446303d75a76ac33dfae335f77
Tarek Ahmed <tarek5701@gmail.com>
2020-11-09 23:25:30 -0800

    Fixed email confirmation bug - added proper configuration for email backend

--- config/email.py
+++ config/email.py
@@ -1,15 +1,14 @@
 from decouple import config
+import os
 
 
-# if config('DEBUG'): 
-#     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# else:
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+if 'RDS_DB_NAME' in os.environ: 
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
+    EMAIL_PORT = config('EMAIL_PORT')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+else:    
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
